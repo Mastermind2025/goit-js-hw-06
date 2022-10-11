984f90fe7665680023fbe1d28f469a788ae91b3b
Mastermind2025 <85400723+Mastermind2025@users.noreply.github.com>
2022-10-11 13:55:25 +0300

    zzz

--- js/task-04.js
+++ js/task-04.js
@@ -8,21 +8,49 @@
 // });
 
 
-let valueEl = Number(document.querySelector('#value').textContent);
+// let valueEl = Number(document.querySelector('#value').textContent);
 
-let counterValue = document.querySelector('#value');
+// let counterValue = document.querySelector('#value');
 
-const decrementBtn = document.querySelector('#counter').firstElementChild;
-const incrementBtn = document.querySelector('#counter').lastElementChild;
+// const decrementBtn = document.querySelector('#counter').firstElementChild;
+// const incrementBtn = document.querySelector('#counter').lastElementChild;
 
-incrementBtn.addEventListener('click', (event) => {
-  valueEl += 1;
+// incrementBtn.addEventListener('click', (event) => {
+//   valueEl += 1;
 
-  counterValue.innerText = valueEl;
-});
+//   counterValue.innerText = valueEl;
+// });
 
-decrementBtn.addEventListener('click', (event) => {
-  valueEl -= 1;
+// decrementBtn.addEventListener('click', (event) => {
+//   valueEl -= 1;
 
-  counterValue.innerText = valueEl;
-});
+//   counterValue.innerText = valueEl;
+// });
+
+
+
+let numberTo = Number(document.querySelector('span#value').innerText);
+let numInWin = document.querySelector('span#value');
+
+
+// console.log(numberTo.innerText);//Text from span
+// console.log(typeof(numberTo.innerText));//This is string
+// console.log(typeof(Number(numberTo.innerText)));//String to number
+// const numThird = Number(numberTo.innerText) + 3;
+// console.log(numThird);// number 3
+
+const firstButton = document.querySelector('div#counter').firstElementChild;
+const lastButton = document.querySelector('div#counter').lastElementChild;
+// console.log(firstButton);
+// console.log(lastButton);
+
+firstButton.addEventListener('click', (event) => {
+    numberTo = numberTo - 1;
+    numInWin.innerText = numberTo;
+})
+
+lastButton.addEventListener('click', (event) => {
+    numberTo = numberTo + 1;
+    numInWin.innerText = numberTo;
+})
+
